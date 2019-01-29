b6ae30319479d0c587b9feeb190cb6e0e30f1a1f
xlnwel <122134545@qq.com>
2019-01-29 15:21:26 +0800

    option for logging params

--- dppo/learner.py
+++ dppo/learner.py
@@ -2,6 +2,7 @@ import numpy as np
 import ray
 
 from agent import Agent
+from utils.np_math import norm
 
 @ray.remote
 class Learner(Agent):
@@ -12,19 +13,29 @@ class Learner(Agent):
                  sess_config=None,
                  reuse=False,
                  save=True,
-                 log_tensorboard=False):
+                 log_tensorboard=False,
+                 log_params=False):
 
+        self.timestep = 0
         super().__init__(name, args, env_args, sess_config=sess_config,
-                         reuse=reuse, save=save, log_tensorboard=log_tensorboard)
+                         reuse=reuse, save=save, 
+                         log_tensorboard=log_tensorboard,
+                         log_params=log_params)
     
     def apply_gradients(self, *grads):
         grads = np.mean(grads, axis=0)
         
         feed_dict = {grad_and_var[0]: grad for grad_and_var, grad in zip(self.grads_and_vars, grads)}
        
-        self.sess.run(self.opt_op, feed_dict=feed_dict)
+        _, summary = self.sess.run([self.opt_op, self.graph_summary], feed_dict=feed_dict)
+
+        self.timestep += 1
+        if self.timestep % 10 == 0:
+            self.writer.add_summary(summary, self.timestep)
+            self.save()
+
         return self.get_weights()
 
     def get_weights(self):
         return self.variables.get_flat()
-        
+     
