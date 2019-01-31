524bff3ca61c7538e95cf184b61a82716b47319f
xlnwel <122134545@qq.com>
2019-01-31 13:45:51 +0800

    Update train.py

--- dppo/train.py
+++ dppo/train.py
@@ -1,5 +1,6 @@
 import os
 import time
+from collections import deque
 import logging
 import numpy as np
 import tensorflow as tf
@@ -10,46 +11,51 @@ from utils import yaml_op, logger
 from worker import Worker
 from learner import Learner
 
+def get_model_name(args, name):
+    option = args['option']
+    option_str = ''
+    for key, value in option.items():
+        option_str = option_str + '{}-{}_'.format(key, value)
+    model_name = option_str + name
+
+    return model_name
 
 def train(ppo_args, env_args):
-    def change_model_name(args, name):
-        args['model_name'] = name
-
     n_workers = ppo_args['n_workers']
     del ppo_args['n_workers']
     ppo_args['batch_size'] //= n_workers
 
-    change_model_name(ppo_args, 'leaner')
-    learner = Learner.remote('ppo', ppo_args, env_args, log_tensorboard=True)
+    ppo_args['model_name'] = get_model_name(ppo_args, 'leaner')
+    learner = Learner.remote('ppo', ppo_args, env_args, log_tensorboard=True, log_params=True, log_score=True)
 
     workers = []
     for i in range(1, n_workers+1):
-        change_model_name(ppo_args, 'worker-{}'.format(i))
+        ppo_args['model_name'] = get_model_name(ppo_args, 'worker-{}'.format(i))
         workers.append(Worker.remote('ppo', ppo_args, env_args))
     
     print('Start Training...\n\n\n')
     
     weights_id = learner.get_weights.remote()
-    
-    for i in range(1, 100 + 1):
+    score_deque = deque(maxlen=100)
+    for i in range(1, 1000 + 1):
         score_ids = [worker.sample_trajectories.remote(weights_id) for worker in workers]
 
         for _ in range(ppo_args['n_updates_per_iteration']):
             grads_ids = [worker.compute_gradients.remote(weights_id) for worker in workers]
-
+            
             weights_id = learner.apply_gradients.remote(*grads_ids)
 
         scores = ray.get(score_ids)
+        score = np.mean(scores)
+        score_deque.append(score)
+        learner.log_score.remote(score, np.mean(score_deque))
         # logger.log_tabular('SamplingTime', sampling_time)
         logger.log_tabular('Iteration', i)
-        logger.log_tabular('AverageScore', np.mean(scores))
+        logger.log_tabular('AverageScore', score)
         logger.log_tabular('StdScore', np.std(scores))
         logger.log_tabular('MaxScore', np.max(scores))
         logger.log_tabular('MinScore', np.min(scores))
         logger.dump_tabular()
-
-        # al_id, cl_id = workers[0].compute_loss.remote()
-        # print('Actor loss:{:>10.2f}, Critic loss:{:>10.2f}'.format(ray.get(al_id), ray.get(cl_id)))
 
 def main():
     args = yaml_op.load_args()
@@ -57,7 +63,7 @@ def main():
     # setup logger
     if not(os.path.exists('data')):
         os.makedirs('data')
-    logdir = args['ppo']['model_name'] + '_' + args['env']['name'] + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    logdir = get_model_name(args['ppo'], 'data') +  '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
     logdir = os.path.join('data', logdir)
 
     logger.configure_output_dir(logdir)
