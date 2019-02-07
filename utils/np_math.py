056ab59aa4d9a3992e76e470ab8430c81befd203
xlnwel <122134545@qq.com>
2019-02-07 23:42:55 +0800

    Update np_math.py

--- utils/np_math.py
+++ utils/np_math.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def norm(x, mean=0., std=1., epsilon=1e-8):
-    normalized_x = (x - np.mean(x)) / (np.std(x) + epsilon)
-    x = normalized_x * std + mean
+def normalize(x, mean=0., std=1., epsilon=1e-8):
+    x = (x - np.mean(x)) / (np.std(x) + epsilon)
+    x = x * std + mean
 
     return x
