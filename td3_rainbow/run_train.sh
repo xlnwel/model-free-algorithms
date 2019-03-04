6439065966e6f749aec335292940a3cef0447368
xlnwel <122134545@qq.com>
2019-03-04 09:07:46 +0800

    Update run_train.sh

--- td3_rainbow/run_train.sh
+++ td3_rainbow/run_train.sh
@@ -1,5 +1,5 @@
 source activate gym
 
-# python train.py
+# train multiple agent
 export OPENBLAS_NUM_THREADS=1
-PYTHONPATH=$(dirname $(pwd)):$PYTHONPATH  xvfb-run -s "-screen 0 1400x900x24" -a python train.py
+PYTHONPATH=$(dirname $(pwd)):$PYTHONPATH  xvfb-run -s "-screen 0 1400x900x24" -a python distributed_train.py
