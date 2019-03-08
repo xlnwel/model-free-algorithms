a1224d83ef68c30d3db84ca86ce0a9af1a4ff11e
xlnwel <122134545@qq.com>
2019-03-08 22:21:34 +0800

    Update distributed_train.py

--- td3_rainbow/distributed_train.py
+++ td3_rainbow/distributed_train.py
@@ -16,22 +16,23 @@ def main():
     agent_args = args['agent']
     buffer_args = args['buffer']
 
-    ray.init(num_gpus=1)
+    ray.init(num_cpus=12, num_gpus=1)
 
     agent_name = 'Agent'
     learner = Learner.remote(agent_name, agent_args, env_args, buffer_args, device='/gpu: 0')
 
     workers = []
+    
     for worker_no in range(agent_args['num_workers']):
-        weight_update_interval = np.random.randint(1, 10) * 1e3
-        worker = Worker.remote(agent_name, worker_no, agent_args, env_args, buffer_args, learner, 
-                                weight_update_interval, device='/cpu: {}'.format(worker_no + 1))
+        local_buffer_capacity = np.random.randint(1, 10) * 1e3
+        worker = Worker.remote(agent_name, worker_no, agent_args, env_args, buffer_args, 
+                    local_buffer_capacity, device='/cpu: {}'.format(worker_no + 1))
         workers.append(worker)
 
-    sample_ids = [worker.sample_data.remote() for worker in workers]
+    pids = [worker.sample_data.remote(learner) for worker in workers]
+
+    ray.get(pids)
     
-    while True:
-        time.sleep(60)
 
 if __name__ == '__main__':
     main()
