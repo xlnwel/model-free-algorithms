5d8e42bb17a27ee6d2dcbd0245cb043f11c1c992
xlnwel <122134545@qq.com>
2019-03-09 22:27:57 +0800

    Update worker.py

--- td3_rainbow/worker.py
+++ td3_rainbow/worker.py
@@ -4,17 +4,7 @@ import ray
 
 from replay.utils import reset_buffer, add_buffer
 from td3_rainbow.agent import Agent
-
-
-class LocalBuffer(dict):
-    def __init__(self, length, **kwarg):
-        self.fake_ratio = np.zeros(length)
-        self.fake_ids = np.zeros(length, dtype=np.int32)
-
-    def __call__(self):
-        while True:
-            yield self.fake_ratio, self.fake_ids, (self['state'], self['action'], self['reward'], 
-                                self['next_state'], self['done'], self['steps'])
+from utility import tf_utils
 
 
 @ray.remote(num_cpus=1)
@@ -26,8 +16,7 @@ class Worker(Agent):
                  args, 
                  env_args,
                  buffer_args,
-                 learner,
-                 weight_update_interval,
+                 store_episodes,
                  sess_config=None, 
                  reuse=None, 
                  save=False, 
@@ -36,11 +25,7 @@ class Worker(Agent):
                  log_score=False,
                  device=None):
         self.no = worker_no
-        self.local_buffer_capacity = int(1e3)
-        self.buffer = LocalBuffer(self.local_buffer_capacity)
-        self.lb_idx = 0
-        self.learner = learner
-        self.weight_update_interval = weight_update_interval
+        buffer_args['store_episodes'] = store_episodes
 
         super().__init__(name, args, env_args,
                          buffer_args=buffer_args,
@@ -52,49 +37,56 @@ class Worker(Agent):
                          log_score=log_score,
                          device=device)
 
-        self._reset_buffer = lambda: reset_buffer(self.buffer, self.local_buffer_capacity + self.n_steps, 
-                                                self._state_dim, self._action_dim, True)
+        self.store_episodes = store_episodes
+        self.local_buffer_capacity = store_episodes * self.max_path_length
+        self.lb_idx = 0
+
+        self._reset_buffer = lambda: reset_buffer(self.buffer, self.local_buffer_capacity, 
+                                                self.state_dim, self.action_dim, True)
         self._reset_buffer()
 
         print('Worker {} has been constructed.'.format(self.no))
 
-    def sample_data(self):
-        i = 0
-        avg_score = deque(maxlen=100)
+    def sample_data(self, learner):
+        # I intend not to synchronize the worker's weights at the beginning for initial exploration 
+        score_deque = deque(maxlen=100)
+        episode_i = 0
+        
         while True:
             state = self.env.reset()
             score = 0
-            for _ in range(self._max_path_length):
-                i += 1
+            
+            for _ in range(self.max_path_length):
                 action = self.act(state)
                 next_state, reward, done, _ = self.env.step(action)
-                score += reward
-
+                
                 add_buffer(self.buffer, self.lb_idx, state, action, reward, 
                             next_state, done, self.n_steps, self.gamma)
+
+                score += reward
+                state = next_state
+
                 self.lb_idx += 1
 
-                if self.lb_idx >= self.local_buffer_capacity + self.n_steps:
-                    priority = self.sess.run(self.priority)
-                    self.buffer['priority'] = priority
-
-                    # buffer_id = ray.put(self.buffer)
-                    self.learner.merge_buffer.remote(dict(self.buffer), self.local_buffer_capacity)
-
-                    self._reset_buffer()
-                    self.lb_idx = 0
-                    break
-                    
                 if done:
                     break
-
-            avg_score.append(score)
-
-            self.learner.log_score.remote(self.no, score, np.mean(avg_score))
             
-            if i > self.weight_update_interval:
-                weights = ray.get(self.learner.get_weights.remote())
-                self.variables.set_flat(weights)
-                i = 0
+            episode_i += 1
+            score_deque.append(score)
+            learner.log_score.remote(self.no, score, np.mean(score_deque))
+            
+            if episode_i >= self.store_episodes:
+                priority = self.sess.run(self.priority)
+                self.buffer['priority'] = priority
 
-        return self.no
+                # buffer_id = ray.put(self.buffer)
+                learner.merge_buffer.remote(dict(self.buffer), self.lb_idx)
+
+                self._reset_buffer()
+                self.lb_idx = 0
+                episode_i = 0
+
+                weights = ray.get(learner.get_weights.remote(self.no))
+                self.variables.set_flat(weights)
+
+        print('Error Reaching')
