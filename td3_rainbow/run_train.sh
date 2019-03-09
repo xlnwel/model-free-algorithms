23b7051601a15f3c6fbebd80c9ba363b4fe975c2
xlnwel <122134545@qq.com>
2019-03-10 07:13:36 +0800

    distributed baseline

--- td3_rainbow/run_train.sh
+++ td3_rainbow/run_train.sh
@@ -1,6 +1,7 @@
 source activate gym
 
-# export TF_CPP_MIN_LOG_LEVEL=2
-# train multiple agent
+export TF_CPP_MIN_LOG_LEVEL=3
 export OPENBLAS_NUM_THREADS=1
+# PYTHONPATH=$(dirname $(pwd)):$PYTHONPATH  xvfb-run -s "-screen 0 1400x900x24" -a python train.py
+# train multiple agent
 PYTHONPATH=$(dirname $(pwd)):$PYTHONPATH  xvfb-run -s "-screen 0 1400x900x24" -a python distributed_train.py
