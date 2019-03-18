d18374f579c7499cbd6e6308a00ddb4a92da93ca
xlnwel <122134545@qq.com>
2019-03-18 21:59:20 +0800

    Add colorize

--- utility/debug_tools.py
+++ utility/debug_tools.py
@@ -1,5 +1,30 @@
 from time import strftime, gmtime, time
 
+color2num = dict(
+    gray=30,
+    red=31,
+    green=32,
+    yellow=33,
+    blue=34,
+    magenta=35,
+    cyan=36,
+    white=37,
+    crimson=38
+)
+
+def colorize(string, color, bold=False, highlight=False):
+    """
+    Colorize a string.
+
+    This function was originally written by John Schulman.
+    """
+    attr = []
+    num = color2num[color]
+    if highlight: num += 10
+    attr.append(str(num))
+    if bold: attr.append('1')
+    return f'\x1b[{";".join(attr)}m{string}\x1b[0m'
+
 def timeit(func, name=None, to_print=False):
 	start_time = gmtime()
 	start = time()
