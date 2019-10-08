338ab3cf4cd518e71281d7f7dc6a16dea5f1c35f
xlnwel <122134545@qq.com>
2019-10-08 09:40:36 +0800

    Add tag

--- utility/plot.py
+++ utility/plot.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utility.debug_tools import assert_colorize, pwc
 
 
-def plot_data(data, x, y, outpath):
+def plot_data(data, x, y, outpath, tag):
     if isinstance(data, list):
         data = pd.concat(data, ignore_index=True)
 
@@ -18,7 +18,10 @@ def plot_data(data, x, y, outpath):
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111)
     sns.set(style="whitegrid", font_scale=1.5)
-    sns.lineplot(x=x, y=y, ax=ax, data=data, hue='Condition')
+    if 'Timing' in data.columns:
+        sns.lineplot(x=x, y=y, ax=ax, data=data, hue=tag, style='Timing')
+    else:
+        sns.lineplot(x=x, y=y, ax=ax, data=data, hue=tag)
     ax.grid(True, alpha=0.3, linestyle=':')
     ax.legend(loc='best').set_draggable(True)
     ax.spines['top'].set_visible(False)
@@ -26,15 +29,15 @@ def plot_data(data, x, y, outpath):
     fig.savefig(outpath)
 
 
-def get_datasets(filedir, condition=None):
+def get_datasets(filedir, tag, condition=None):
     unit = 0
     datasets = []
     for root, _, files in os.walk(filedir):
         if 'log.txt' in files:
             log_path = os.path.join(root, 'log.txt')
-            data = pd.read_table(log_path)
+            data = pd.read_csv(log_path, sep='\t')
 
-            data.insert(len(data.columns), 'Condition', condition)
+            data.insert(len(data.columns), tag, condition)
 
             datasets.append(data[:80])
             unit +=1
@@ -47,6 +50,7 @@ def main():
     parser.add_argument('logdir', nargs='*')
     parser.add_argument('--outname', '-o')
     parser.add_argument('--legend', nargs='*')
+    parser.add_argument('--legendtag', '-t', default='Algo')
     parser.add_argument('--x', default='Iteration', nargs='*')
     parser.add_argument('--y', default='ScoreMean', nargs='*')
     args = parser.parse_args()
@@ -64,14 +68,17 @@ def main():
         legends = args.legend
     else:
         legends = [os.path.basename(path) for path in dirs]
+    tag = args.legendtag
 
     pwc('Directories:')
-    pwc(dirs)
+    for d in dirs:
+        pwc(f'\t{d}')
     pwc('Legends:')
-    pwc(legends)
+    for l in legends:
+        pwc(f'\t{l}')
     data = []
     for logdir, legend_title in zip(dirs, legends):
-        data += get_datasets(logdir, legend_title)
+        data += get_datasets(logdir, tag, legend_title)
 
     if isinstance(args.x, list) or isinstance(args.y, list):
         xs = args.x if isinstance(args.x, list) else [args.x]
@@ -79,10 +86,10 @@ def main():
         for x in xs:
             for y in ys:
                 outpath = f'results/{args.outname}/{x}-{y}.png'
-                plot_data(data, x, y, outpath)
+                plot_data(data, x, y, outpath, tag)
     else:
         outpath = f'results/{args.outname}.png'
-        plot_data(data, args.x, args.y, outpath)
+        plot_data(data, args.x, args.y, outpath, tag)
 
 if __name__ == '__main__':
     main()
